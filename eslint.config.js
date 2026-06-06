e869022b51eb792e8c6daec21a962bd82163216d
Shubham Taywade <shubhamtaywade82@gmail.com>
2026-06-06 15:35:36 +0530

    feat(eslint): add custom rules for TypeScript linting
    - Introduced rules to enforce no unused variables, allowing exceptions for variables prefixed with an underscore.
    - Configured the 'no-empty' rule to permit empty catch blocks, enhancing error handling flexibility in TypeScript code.

--- eslint.config.js
+++ eslint.config.js
@@ -19,5 +19,16 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+      'no-empty': ['error', { allowEmptyCatch: true }],
+    },
   },
 ])
